1a14b11173f62673fc0d1e64fa3662bd6e36e91b
ity0526 <61374973+ity0526@users.noreply.github.com>
2023-04-10 23:18:54 +0900

    230410

--- todo.js
+++ todo.js
@@ -1,47 +1,73 @@
 const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
-const localTodo = localStorage.getItem("Todolist");
+
+
+const TODOLIST = "Todolist"
+
+let localTodo = localStorage.getItem(TODOLIST);
 
 function todoSubmit(event) {
-	event.preventDefault();
-	const todo = todoInput.value;
-	const li = document.createElement("li");
-	li.innerText = todo;
-	todoList.appendChild(li);
-	
-	const button = document.createElement("button");
-	button.innerText = "X";
-	button.type = "click";
-	li.appendChild(button);
-	
-	saveTodo(todo);
-	
-	todoInput.value = "";
+	if(todoInput.value !== ""){
+		console.log(todoInput.value);
+		event.preventDefault();
+		const todo = todoInput.value;
+		const li = document.createElement("li");
+		li.innerText = todo;
+		todoList.appendChild(li);
+
+		const button = document.createElement("button");
+		button.innerText = "X";
+		button.className = "remove";
+		button.addEventListener("click", removeTodo)
+		li.appendChild(button);
+
+		saveTodo(todo);
+
+		todoInput.value = "";
+	}else{
+		alert("Please write Todo");
+	}
 }
 
-function saveTodo(todo) {
+function saveTodo(todo) { 
 		
 	if(localTodo !== null){
 		const newTodo = JSON.parse(localTodo);
 		newTodo.push(todo);
-		localStorage.setItem("Todolist", JSON.stringify(newTodo))
+		localTodo = JSON.stringify(newTodo);
+		localStorage.setItem(TODOLIST, localTodo);
 	}else{
 		const firstTodo = [];
 		firstTodo.push(todo);
-		localStorage.setItem("Todolist", JSON.stringify(firstTodo));
+		localTodo = JSON.stringify(firstTodo);
+		localStorage.setItem(TODOLIST, localTodo);
 	}
 }
 
-function reloadTodo(todo){
-		const li = document.createElement("li");
-		li.innerText = todo;
-		todoList.appendChild(li);
-		
-		const button = document.createElement("button");
-		button.innerText = "X";
-		button.type = "click";
-		li.appendChild(button);
+function removeTodo(event) {
+	const removeTarget = event.currentTarget.parentNode;
+	
+	const todo = localStorage.getItem(TODOLIST);
+	const newTodo = JSON.parse(todo).remove(removeTarget.innerText);
+	localStorage.setItem(TODOLIST, JSON.stringify(newTodo));
+	
+	todoList.removeChild(removeTarget);
 }
+
+function reloadTodo(todo){
+	const li = document.createElement("li");
+	li.innerText = todo;
+	todoList.appendChild(li);
+		
+	const button = document.createElement("button");
+	button.innerText = "X";
+	button.className = "remove";
+	button.addEventListener("click", removeTodo)
+	li.appendChild(button);
+}
+
 document.addEventListener("submit", todoSubmit);
 if(localTodo !== null) JSON.parse(localTodo).forEach(reloadTodo);
+
+//객체를 이용해서 localStorage에 저장.
